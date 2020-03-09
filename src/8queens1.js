b9748334f32cf7996b0b880bb4f33f6a61bb3b85
Sergio Tabares <alu0101124896.ull.edu.es>
2020-03-09 20:15:51 +0000

    Starting 8queens1.js

---
+++ src/8queens1.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @file 8queens2.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Winter 2020
+ * College: University of La Laguna
+ * Course: Computer Science - Interactive Aplication Programing
+ * Description: This program calculates all posible combinations of the 8 queens problem
+ */
+
+/**
+ * Funcion que comprueba que la combinacion sea valida
+ *
+ * @param {*} params - 
+ * @returns {}
+ */
+function isSolution(params) {
+  
+}
+
+/**
+ * Funcion principal
+ *
+ * @param {string} pname - pdesc.
+ * @returns {} - iytfd ?
+ */
+function main() {
+  if (process.argv.length !== 3 || isNaN(process.argv[2])) {
+    console.log('Error: Ejecute este programa aportando como argumento en la linea de comandos exactamente un numero entero.');
+  } else {
+    
+  }
+}
+
+main();
